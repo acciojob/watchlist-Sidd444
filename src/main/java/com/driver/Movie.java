e61f69ffe76e4259aee175086669e091e51021a0
Sidd444 <siddharthabharaliassam@gmail.com>
2023-06-03 23:06:14 +0530

    he likes to movie movie

--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -1,10 +1,10 @@
 package com.driver;
 
 public class Movie {
-    String name;
-    int durationInMinutes;
-    double imdbRating;
-    Movie( String name, int durationInMinutes, double imdbRating){
+    private String name;
+    private int durationInMinutes;
+    private double imdbRating;
+   public Movie( String name, int durationInMinutes, double imdbRating){
         this.name=name;
         this.durationInMinutes= durationInMinutes;
         this.imdbRating =imdbRating;
